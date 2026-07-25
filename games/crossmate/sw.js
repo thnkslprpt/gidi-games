22a8c94dac26e4ca68306f2ee06a85fb4240648a
Avi <9024662+thnkslprpt@users.noreply.github.com>
2026-07-25 21:26:36 +0300

    update

---
+++ games/crossmate/sw.js
@@ -0,0 +1,61 @@
+/* Crossmate v1.5.0 offline cache */
+"use strict";
+
+var CACHE_PREFIX = "crossmate-";
+var CACHE_NAME = "crossmate-v1.5.0";
+var CORE = ["./", "./index.html", "./firebase-config.js"];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return Promise.all(CORE.map(function (url) {
+        return cache.add(url).catch(function () { return undefined; });
+      }));
+    }).then(function () { return self.skipWaiting(); })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (names) {
+      return Promise.all(names.map(function (name) {
+        if (name.indexOf(CACHE_PREFIX) === 0 && name !== CACHE_NAME) { return caches.delete(name); }
+        return undefined;
+      }));
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  var request = event.request;
+  var url;
+  if (request.method !== "GET") { return; }
+  url = new URL(request.url);
+  if (request.mode === "navigate") {
+    event.respondWith(
+      fetch(request).then(function (response) {
+        var copy = response.clone();
+        caches.open(CACHE_NAME).then(function (cache) { cache.put("./index.html", copy); });
+        return response;
+      }).catch(function () {
+        return caches.match("./index.html").then(function (cached) {
+          return cached || caches.match("./");
+        });
+      })
+    );
+    return;
+  }
+  if (url.origin !== self.location.origin || url.pathname.indexOf("/games/crossmate/") === -1) { return; }
+  event.respondWith(
+    caches.match(request, { ignoreSearch: true }).then(function (cached) {
+      if (cached) { return cached; }
+      return fetch(request).then(function (response) {
+        if (response && response.ok) {
+          var copy = response.clone();
+          caches.open(CACHE_NAME).then(function (cache) { cache.put(request, copy); });
+        }
+        return response;
+      });
+    })
+  );
+});
