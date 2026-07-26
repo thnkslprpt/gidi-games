abdf061c8ac6c853caa49d193bba4af928f0e417
Avi <9024662+thnkslprpt@users.noreply.github.com>
2026-07-26 06:50:26 +0300

    update

--- games/crossmate/sw.js
+++ games/crossmate/sw.js
@@ -1,8 +1,8 @@
-/* Crossmate v1.5.0 offline cache */
+/* Crossmate v1.5.1 offline cache */
 "use strict";
 
 var CACHE_PREFIX = "crossmate-";
-var CACHE_NAME = "crossmate-v1.5.0";
+var CACHE_NAME = "crossmate-v1.5.1";
 var CORE = ["./", "./index.html", "./firebase-config.js"];
 
 self.addEventListener("install", function (event) {
